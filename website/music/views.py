0485d3aae208f54f265545bf4fb668e8efb729aa
yassir <roni.yassir@gmail.com>
2019-01-02 19:33:19 +0600

    added index html

--- website/music/views.py
+++ website/music/views.py
@@ -1,13 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = 'music/' + str(album.id) + '/'
-        html += '<a href="' +url+ '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    context = {'all_albums' : all_albums}
+    return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id) :
